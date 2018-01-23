fa3e661d40cf2b1e2692cc3e7db7351493ee6cd0
MaGo1024 <olumideodusote@gmail.com>
2018-01-23 13:39:49 +0100

    Hands-On: Checking Null Values in JavaScript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-var $elem = document.getElementById('output');
-$elem.innerHTML +=  4+6 + 'Demo'
-$elem.innerHTML += 'Demo ' + 4 + 6;
-$elem.innerHTML += 'Demo ' + (4 + 6);
+testVariable ; 
+
+var isUndefined = (testVariable === undefined);
+var isNull = (testVariable === null);
+var isEmpty = (testVariable === '');
+
+document.getElementById('isUndefined').innerHTML = isUndefined;
+document.getElementById('isNull').innerHTML = isNull;
+document.getElementById('isEmpty').innerHTML = isEmpty;
