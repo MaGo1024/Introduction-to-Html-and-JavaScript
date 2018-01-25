86621a65be9de77271d0c80deb5ad3902f7c82e6
MaGo1024 <olumideodusote@gmail.com>
2018-01-25 15:57:20 +0100

    Hands-On: Handling Callbacks

--- js/script.js
+++ js/script.js
@@ -1,28 +1,14 @@
-function saveToCookie(key, value, onCompletion){
-    var  cookieValue = key + ';' + value;
-    document.cookie +=  cookieValue;
-    onCompletion(cookieValue);
-}
-
-function saveTextValue(){
-    var textValue = document.getElementsByName('position')[0].value;
-    saveToCookie('Position', textValue, showTextSuccessful);
-}
-
-function  showTextSuccessful(result){
-    window.alert('You succesfully saved [' + result + '] from the text input to your cookie');
-}
-
-function saveRadioValue(){
-    var radioValue;
-    var radioOption = document.getElementsByName('department');
-    for (var index = 0; index < radioOption.length-1; index++){
-        if (radioOption[index].checked){
-            radioValue = radioOption[index].value;
-            break;
-        }
+function handleClick(callback) {
+    alert('This button has been clicked');
+    if (callback) {
+        callback();
     }
-    saveToCookie('Department', radioValue, function (result){
-        window.alert('You did it! You saved [ ' + result + ']');
-    }); 
+}
+
+function doMore() {
+    alert('I could process more logic here');
+}
+
+function doSomethingElse() {
+    document.writeln('Test Message');
 }
