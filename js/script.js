e971c29c5bb4e55cb6b1ca0f048b7f184b12d739
MaGo1024 <olumideodusote@gmail.com>
2018-01-25 20:05:01 +0100

    canvas

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
-function correctMethodName() {  
-    return "Output Message";
-}
+var canvas = document.getElementById("demoCanvas");
+var context = canvas.getContext("2d");
 
-try {
-    var result = wrongMethodName(3);
-    document.writeln(result);
-}
-catch (error) {
-    /*document.writeln('An error occurred');*/
-    document.writeln('<h3>Error:&nbsp;</h3><p>' + error + "</p>"); 
-}
+context.fillStyle = "#A9B0B3";
+context.strokeStyle = "#746C73";
+context.lineWidth = 5;
 
-document.writeln(result);
+context.moveTo(0, 0);
+context.lineTo(700, 350);
+context.stroke();
 
+context.beginPath();
+context.arc(160, 80, 70, 0, 2 * Math.PI);
+context.fill();
+context.stroke();
+
+context.beginPath();
+context.moveTo(650, 345);
+context.lineTo(700, 350);
+context.lineTo(665, 315);
+context.stroke();
+
+context.fillStyle = "#20293F";
+context.strokeStyle = "#20293F";
+
+context.font = "78px Segoe UI";
+context.fillText("Hello", 190, 230);
+context.strokeText("World", 190, 310);
