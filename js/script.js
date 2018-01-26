c46832d308df7cae7e2afec9b67a44966f8253f9
MaGo1024 <olumideodusote@gmail.com>
2018-01-26 18:47:47 +0100

    Hands-On: Manipulating the DOM using jQuery

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-var socket = new WebSocket('ws://echo.websocket.org');
+$(document).ready(function () {
+    $(document).ready(function () {
+        manipulateDOM();
+    });
+});
 
-socket.onopen = function () {
-    console.log('Our socket has been opened!');
-}
-
-socket.onmessage = function (event) {
-    window.alert(event.data);
-}
-
-function testMessage () {
-    socket.send("Hello World!");
+function manipulateDOM() {
+    var h1Headers = $('h1');
+    h1Headers.text('Hello World');
+    var h3Headers = $('h3');
+    h3Headers.css('color', '#37887D');
+    h3Headers.first().css('text-decoration', 'line-through');
+    var lastH3Header = $('h3:last');
+    lastH3Header.css('background-color', '#53226A');  
 }
